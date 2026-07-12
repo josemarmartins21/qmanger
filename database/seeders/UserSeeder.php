937f8fa803b44b5e038eb2990cd8a82f60ec5018
josemarmartins21 <josemar21@outlook.pt>
2026-07-12 11:56:57 +0100

    chore: add criação do user Deodato

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,5 +25,16 @@ class UserSeeder extends Seeder
         ])->first();
 
         $user->assignPermission('super-admin');
+
+        $user = User::factory(1)->create([
+            'name' => 'Deodato Francisco',
+            'email' => 'deodato@email.pt',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_master' => true,
+        ])->first();
+
+        $user->assignPermission('super-admin');
     }
 }
